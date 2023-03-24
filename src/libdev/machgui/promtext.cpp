WiredForWar/machines-archived

commit 186a947a84d252069298e48cbf5cf23ace91773c

Move non ASCII / UTF-8 symbols (indices) to GuiBmpFont constants

src/libdev/machgui/promtext.cpp
@@ -574,16 +574,16 @@ bool MachPromptText::doHandleKeyEvent( const GuiKeyEvent& event )
 						switch ( chatMessageIntendedForRace_ )
 						{
 							case MachPhys::RED:
-								chatMessageIntendedForStr_ = "©";
+								chatMessageIntendedForStr_ += GuiBmpFont::redCharIndex();
 								break;
 							case MachPhys::GREEN:
-								chatMessageIntendedForStr_ = "ª";
+								chatMessageIntendedForStr_ += GuiBmpFont::greenCharIndex();
 								break;
 							case MachPhys::BLUE:
-								chatMessageIntendedForStr_ = "«";
+								chatMessageIntendedForStr_ += GuiBmpFont::blueCharIndex();
 								break;
 							case MachPhys::YELLOW:
-								chatMessageIntendedForStr_ = "¬";
+								chatMessageIntendedForStr_ += GuiBmpFont::yellowCharIndex();
 								break;
 						}
 						GuiResourceString sendToStr( IDS_SENDTO );
@@ -657,16 +657,16 @@ bool MachPromptText::doHandleKeyEvent( const GuiKeyEvent& event )
 						switch ( MachGuiInGameChatMessages::instance().playerRace() )
 						{
 							case MachPhys::RED:
-								chatMessageStr += "©";
+								chatMessageStr += GuiBmpFont::redCharIndex();
 								break;
 							case MachPhys::GREEN:
-								chatMessageStr += "ª";
+								chatMessageStr += GuiBmpFont::greenCharIndex();
 								break;
 							case MachPhys::BLUE:
-								chatMessageStr += "«";
+								chatMessageStr += GuiBmpFont::blueCharIndex();
 								break;
 							case MachPhys::YELLOW:
-								chatMessageStr += "¬";
+								chatMessageStr += GuiBmpFont::yellowCharIndex();
 								break;
 						}
 						chatMessageStr += MachGuiInGameChatMessages::instance().playerName();

src/libdev/gui/font.cpp
@@ -120,6 +120,25 @@ void GuiBmpFont::releaseFontMemory()
 	fonts.erase( fonts.begin(), fonts.end() );
 }
 
+char GuiBmpFont::redCharIndex()
+{
+	return 0xA9;
+}
+
+char GuiBmpFont::greenCharIndex()
+{
+	return 0xAA;
+}
+
+char GuiBmpFont::blueCharIndex()
+{
+	return 0xAB;
+}
+
+char GuiBmpFont::yellowCharIndex()
+{
+	return 0xAC;
+}
 
 //static
 GuiBmpFont GuiBmpFont::getFont( const SysPathName& fontPath, FontType fontType /*= PROPORTIONAL*/, size_t spaceCharWidth /*= 7*/, size_t spacing /*= 1*/ )

src/libdev/gui/font.hpp
@@ -71,6 +71,11 @@ public:
 	// Free's up all the memory used by the cached fonts.
 	static void releaseFontMemory();
 
+	static char redCharIndex();
+	static char greenCharIndex();
+	static char blueCharIndex();
+	static char yellowCharIndex();
+
 protected:
     GuiBmpFont( const SysPathName& fontPath );
 	void drawTextLeftJustify( const string& text, const Gui::Coord& startPos, int maxWidth ) const;

commit 592a9c7584b3d2485c69a24c154b146b26feeac7

Fix chat message text accumulated and never cleared
Regression after 186a947a84d252069298e48cbf5cf23ace91773c.

src/libdev/machgui/promtext.cpp
@@ -5,6 +5,7 @@
 
 //  Definitions of non-inline non-template methods and global functions
 
+#include "base/prepost.hpp"
 #include "ctl/list.hpp"
 #include "ctl/pvector.hpp"
 #include "machgui/gui.hpp"
@@ -560,16 +561,19 @@ bool MachPromptText::doHandleKeyEvent(const GuiKeyEvent& event)
                         switch (chatMessageIntendedForRace_)
                         {
                             case MachPhys::RED:
-                                chatMessageIntendedForStr_ += GuiBmpFont::redCharIndex();
+                                chatMessageIntendedForStr_.assign(1, GuiBmpFont::redCharIndex());
                                 break;
                             case MachPhys::GREEN:
-                                chatMessageIntendedForStr_ += GuiBmpFont::greenCharIndex();
+                                chatMessageIntendedForStr_.assign(1, GuiBmpFont::greenCharIndex());
                                 break;
                             case MachPhys::BLUE:
-                                chatMessageIntendedForStr_ += GuiBmpFont::blueCharIndex();
+                                chatMessageIntendedForStr_.assign(1, GuiBmpFont::blueCharIndex());
                                 break;
                             case MachPhys::YELLOW:
-                                chatMessageIntendedForStr_ += GuiBmpFont::yellowCharIndex();
+                                chatMessageIntendedForStr_.assign(1, GuiBmpFont::yellowCharIndex());
+                                break;
+                            default:
+                                ASSERT_FAIL("Invalid race for a chat message");
                                 break;
                         }
                         GuiResourceString sendToStr(IDS_SENDTO);