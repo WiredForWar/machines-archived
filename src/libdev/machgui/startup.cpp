WiredForWar/machines-archived

commit c3e035c77d9ef2c5eac9538d4427d2dc846cf31a

Startup: Clear the screen for postload animation
Fixes the empty progress bar rendered behind the video.

src/libdev/machgui/startup.cpp
@@ -2340,6 +2340,14 @@ void MachGuiStartupScreens::contextAnimation()
                 flicExists = true;
             }
         }
+
+        if (flicExists)
+        {
+            desiredCdTrack_ = DONT_PLAY_CD;
+
+            // Clear the entire screen to stop previous screen from showing through black borders.
+            RenDevice::current()->clearAllSurfaces(RenColour::black());
+        }
     }
     else if (context_ == CTX_ENTRYFLIC or context_ == CTX_VICTORYFLIC or context_ == CTX_DEFEATFLIC)
     {

commit 40692b856f53abb65f691c84744f54bb65c8559a

Fix the post-loading music (regression)
The music change was accidentally introduced by commit c3e035c77d9ef2c.

src/libdev/machgui/startup.cpp
@@ -2350,8 +2350,6 @@ void MachGuiStartupScreens::contextAnimation()
 
         if (flicExists)
         {
-            desiredCdTrack_ = DONT_PLAY_CD;
-
             // Clear the entire screen to stop previous screen from showing through black borders.
             RenDevice::current()->clearAllSurfaces(RenColour::black());
         }